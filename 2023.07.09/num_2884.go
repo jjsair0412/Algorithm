48ac0d5313e0c073c48ef52c73bd56c93317cc2c
jinseong <jjsair0412@naver.com>
2023-07-10 13:48:12 +0900

    2884 init

--- 2023.07.09/num_2884.go
+++ 2023.07.09/num_2884.go
@@ -8,8 +8,6 @@ func main() {
 		M int
 	)
 
-	var tmp int = 24
-
 	_, err := fmt.Scanf("%d", &H)
 	if err != nil {
 		fmt.Println("입력 에러 !")
@@ -24,7 +22,16 @@ func main() {
 
 	if H >= 0 && M >= 0 {
 		if H <= 23 && M <= 59 {
-
+			if M < 45 {
+				H -= 1
+				M = 60 - (45 - M)
+			} else if M >= 45 {
+				M -= 45
+			}
+			if H < 0 {
+				H += 24
+			}
+			fmt.Printf("%d %d", H, M)
 		}
 	} else {
 		fmt.Println("잘못된 값을 입력하셨습니다.")
