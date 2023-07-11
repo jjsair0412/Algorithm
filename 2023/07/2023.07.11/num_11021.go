161229b80efd8f610f398cb30e5493dfe785f524
jinseong <jjsair0412@naver.com>
2023-07-11 21:10:58 +0900

    go work 파일 추가 및 11021 solve

--- 2023/07/2023.07.11/num_11021.go
+++ 2023/07/2023.07.11/num_11021.go
@@ -14,14 +14,27 @@ func main() {
 	)
 
 	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
 
-	fmt.Fscanf(reader, "%d\n", &count)
-
-	for i := 0; i < count; i++ {
-		fmt.Fscanf(reader, "%d %d\n", &A, &B)
-		fmt.Fprintf(writer, "Case #%d: %d\n", i, A+B)
+	_, err := fmt.Fscanf(reader, "%d\n", &count)
+	if err != nil {
+		fmt.Println("입력 에러 발생")
+		return
 	}
 
-	writer.Flush()
+	for i := 1; i <= count; i++ {
+		_, err := fmt.Fscanln(reader, &A, &B)
+		if err != nil {
+			fmt.Println("for문 내부 입력에러 발생")
+			fmt.Println(err)
+			return
+		}
+		result := A + B
+		_, err = fmt.Fprintf(os.Stdout, "Case #%d: %d\n", i, result)
+		if err != nil {
+			fmt.Println("for문 내부 출력에러 발생")
+			fmt.Println(err)
+			return
+		}
+
+	}
 }
