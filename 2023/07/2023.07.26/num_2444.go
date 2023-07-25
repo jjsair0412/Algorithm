824abec82f86356e202ae54b1023868b2223df74
jjsair0412 <jjsair0412@naver.com>
2023-07-26 01:17:25 +0900

    2444 init

---
+++ 2023/07/2023.07.26/num_2444.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var N int
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Fscanf(reader, "%d\n", &N)
+
+	for i := 0; i < N*2; i++ {
+
+	}
+
+}
